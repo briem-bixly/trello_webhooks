162e8e64f00188e7822d30649f28f78df24368c9
Fernando Sanchez, Jr. <fernandobixly@users.noreply.github.com>
2015-06-24 14:14:15 -0700

    Update trello_webhook_setup.py

--- scripts/trello_webhook_setup.py
+++ scripts/trello_webhook_setup.py
@@ -1,11 +1,13 @@
 class trello_webhook_setup(NebriOS):
     listens_to = ['trello_webhook_setup']
+    required = ['trello_api_key', 'trello_api_secret', 'instance_name']
     
     # Note: This script is used to set up the trello webhook system.
     # If shared.TRELLO_API_KEY and shared.TRELLO_API_SECRET are not created,
     # you should supply them like so:
     # trello_api_key := <api_key>
     # trello_api_secret := <api_secret>
+    # instance_name := <instance_name>
 
     def check(self):
         return self.trello_webhook_setup == True
@@ -14,9 +16,9 @@ class trello_webhook_setup(NebriOS):
         self.trello_webhook_setup = "Ran"
         # check for existance of callback urls
         if shared.TRELLO_WEBHOOK_MEMBER_CALLBACK_URL is None:
-            shared.TRELLO_WEBHOOK_MEMBER_CALLBACK_URL = 'https://francois.nebrios.com/api/v1/trello_webhook/member_callback'
+            shared.TRELLO_WEBHOOK_MEMBER_CALLBACK_URL = 'https://%s.nebrios.com/api/v1/trello_webhook/member_callback' % self.instance_name
         if shared.TRELLO_WEBHOOK_BOARD_CALLBACK_URL is None:
-            shared.TRELLO_WEBHOOK_BOARD_CALLBACK_URL = 'https://francois.nebrios.com/api/v1/trello_webhook/board_callback'
+            shared.TRELLO_WEBHOOK_BOARD_CALLBACK_URL = 'https://%s.nebrios.com/api/v1/trello_webhook/board_callback' % self.instance_name
         # check for existance of trello api key/secret
         if shared.TRELLO_API_KEY is None:
             if self.trello_api_key is not None:
